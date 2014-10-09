557ef36a67872aeb683478ed66b7ea07be91fc0a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-10-09 15:47:01 +0200

    Add a tilevector source

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var map;
 
 function init() {
+  tile_source = new ol.source.TileVector({
+    format: new ol.format.GeoJSON({
+    }),
+    url: "/test.cgi?x={x}&y={y}&zoom={z}&tilesize=1024&srs=3857",
+    tileGrid: new ol.tilegrid.XYZ({
+      maxZoom: 17,
+      tileSize: 1024
+    }),
+  });
+
+  vector_layer = new ol.layer.Vector({
+    source: tile_source
+  });
+
   map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({
-        source: new ol.source.MapQuest({layer: 'sat'})
-      })
+        source: new ol.source.OSM()
+      }),
+      vector_layer
     ],
     view: new ol.View({
-      center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-      zoom: 4
+      center: ol.proj.transform([16.41, 48.20], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 16
     })
   });
 }
