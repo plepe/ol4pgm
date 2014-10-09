083029fe6908f60a5df95f0305b26af1e54908be
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-10-09 12:53:54 +0200

    Reformat, put into a function which is called from window.onload

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var map = new ol.Map({
-target: 'map',
-layers: [
-  new ol.layer.Tile({
-    source: new ol.source.MapQuest({layer: 'sat'})
-  })
-],
-view: new ol.View({
-  center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-  zoom: 4
-})
-});
+var map;
+
+function init() {
+  map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.MapQuest({layer: 'sat'})
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 4
+    })
+  });
+}
+
+window.onload = init;
