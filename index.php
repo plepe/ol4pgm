ae9084068bee5802d336ed465ec70dd3dcc38a55
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-10-09 12:51:55 +0200

    Initial import from ol3 example

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="http://openlayers.org/en/v3.0.0/css/ol.css" type="text/css">
+    <style>
+      .map {
+        height: 400px;
+        width: 100%;
+      }
+    </style>
+    <script src="http://openlayers.org/en/v3.0.0/build/ol.js" type="text/javascript"></script>
+    <title>OpenLayers 3 for pgmapcss example</title>
+  </head>
+  <body>
+    <h2>My Map</h2>
+    <div id="map" class="map"></div>
+    <script type="text/javascript" src="index.js"></script>
+  </body>
+</html>
