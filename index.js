8fc1200f51684e1ea9bcdbbbc0ddc98c7b2ac1fa
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-05-10 07:05:21 +0200

    Update URL via History API

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ function init() {
     tileSize: 1024
   }, map);
 
+  map.on('moveend', function() {
+    var center = ol.proj.transform(map.getView().getCenter(), 'EPSG:3857', 'EPSG:4326');
+    var zoom = map.getView().getZoom();
+
+    history.replaceState(null, null, "?lon=" + center[0].toFixed(5) + "&lat=" + center[1].toFixed(5) + "&zoom=" + zoom);
+
+  }.bind(layer));
 }
 
 window.onload = init;
